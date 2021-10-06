ddcb535d1f77751bbd49349b091d4853517e2503
Gabriel Lantin <mushymanee@gmail.com>
2021-10-06 06:57:47 -0700

    basic playback

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// var request = new XMLHttpRequest();
+
+// request.open('GET', "https://api.lyrics.ovh/v1/Drake/God's Plan");
+
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log('Status:', this.status);
+//     console.log('Headers:', this.getAllResponseHeaders());
+//     console.log('Body:', this.responseText);
+//   }
+// };
+
+// request.send();
+
+// 425157-EzVibez-ON3O5RLK
+
+// https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction
