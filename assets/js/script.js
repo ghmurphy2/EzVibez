4d8a74dffeaf0541c76419408b7e61864254989f
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-06 20:13:54 -0700

    itunes api variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+var request = new XMLHttpRequest();
+
+// request.open('GET', "https://api.lyrics.ovh/v1/Drake/God's Plan");
+
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log('Status:', this.status);
+//     console.log('Headers:', this.getAllResponseHeaders());
+//     console.log('Body:', this.responseText);
+//   }
+// };
+
+// request.send();
+
+// 425157-EzVibez-ON3O5RLK
+
+// https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction
 // EzVibez is an app that allows user to look up a song(based on parameters) and view the lyrics and a brief analysis of the lyrics based on sentiment
 // layout will be multi paged with a landing page that contians a search 
 // possible song play back spotify or shazam
@@ -27,4 +44,20 @@
         // query sentiment
             // set mood 
 
-// 
+            Where parameterkeyvalue can be one or more parameter key and value pairs indicating the details of your query.
+
+            To construct a parameter key and value pair, you must concatenate each parameter key with an equal sign (=) and a value string. For example: key1=value1. To create a string of parameter key and value pairs, you must concatenate each pair using an ampersand (&). For example: key1=value1&key2=value2&key3=value3.
+            Country code US
+            music terms musicArtist, musicTrack, album, musicVideo, mix, song.
+            <script src="https://.../search?parameterkeyvalue&callback="{name of JavaScript function in webpage}"/>.
+            Please note that “musicTrack” can include both songs and music videos in the results.
+            
+            To search for all Jack Johnson audio and video content and return only the first 25 items, use the following URL: https://itunes.apple.com/search?term=jack+johnson&limit=25.
+            term=jack+johnson
+            limit=25
+            
+            curl 'https://api.meaningcloud.com/sentiment-2.1' \
+                -F 'key=YOUR API KEY' \
+                -F 'txt=YOUR TEXT HERE' \variable for query, lyric output from ovhs
+                -F 'lang=TEXT LANGUAGE HERE'
+            add text translation
