36c3a75d8a36b38667e6087b331bc09c72f21ec9
Gavin Murphy <gavinhmurphy@gmail.com>
2021-10-07 10:46:11 -0700

    Fresh code format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,107 @@
-var request = new XMLHttpRequest();
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const result = document.getElementById("result");
 
-// request.open('GET', "https://api.lyrics.ovh/v1/Drake/God's Plan");
+// API URL
+const apiURL = "https://api.lyrics.ovh";
 
-// request.onreadystatechange = function () {
-//   if (this.readyState === 4) {
-//     console.log('Status:', this.status);
-//     console.log('Headers:', this.getAllResponseHeaders());
-//     console.log('Body:', this.responseText);
-//   }
-// };
+// Get Search Value
+// add an eventlistener, so everytime the search button is clicked without a value, it will send out an alert to add a value
+form.addEventListener("submit", e => {
+    //  prevent default to prevent from reloading
+    e.preventDefault();
+    // to remove white space from ends of a string
+    searchValue = search.value.trim();
 
-// request.send();
+    if (!searchValue) { 
+        // cant use alerts 
+        alert("Enter an artist or song");
+        // or else I want to begin search
+    } else {
+        beginSearch(searchValue);
+    }
+})
+
+// An async function is a function declared with the async keyword, and the await keyword is permitted within them. The async and await keywords enable asynchronous, promise-based behavior to be written in a cleaner style, avoiding the need to explicitly configure promise chains.
+// Search function 
+
+async function beginSearch(searchValue) {
+    // fetch the results using the api URL 
+    const searchResult = await fetch(`${apiURL}/suggest/${searchValue}`);
+    // we are suppose to get the data from API in json format
+    const data = await searchResult.json();
+    // console.log(data);
+    displayData(data);
+}
+
+// Display Search Result - DisplayData function
+
+function displayData(data) {
+    // create HTML in js
+    // create the result as a list 
+    // .map to create a new array 
+    // we are getting the results and the result is the song, artist name and song title 
+    // create a button get lyrics to expand to get lyrics
+    result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map(song=> `<li>
+                    <div>
+                        <strong>${song.artist.name}</strong> -${song.title} 
+                    </div>
+                    <span data-artist="${song.artist.name}" data-songtitle="${song.title}">Lyrics</span>
+                </li>`
+        )
+        // to join as a whole string
+        .join('')}
+    </ul>
+  `;
+}
+
+//event listener to get the lyrics button
+result.addEventListener('click', e=>{
+    // create a variable called clicked element
+    const clickedElement = e.target;
+
+    //checking if lyrics is a button or not
+    if (clickedElement.tagName === 'SPAN'){
+        const artist = clickedElement.getAttribute('data-artist');
+        const songTitle = clickedElement.getAttribute('data-songtitle');
+        
+        // calling upon a function with two parameters
+        getLyrics(artist, songTitle)
+    }
+})
+
+
+// Get lyrics for the song
+async function getLyrics(artist, songTitle) {
+    // fetch something from the API, artist and song title
+    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    // again needs to be in json format
+    const data = await response.json();
+  
+    // (/(\r\n|\r|\n)/g, '<br>') got this from the website
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  
+    // display lyrics to the 
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <p>${lyrics}</p>`;
+  
+  }
+
+
+
+
+
+
+
+
 
 // 425157-EzVibez-ON3O5RLK
 
 // https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction
+
 // EzVibez is an app that allows user to look up a song(based on parameters) and view the lyrics and a brief analysis of the lyrics based on sentiment
 // layout will be multi paged with a landing page that contians a search 
 // possible song play back spotify or shazam
@@ -43,5 +130,3 @@ var request = new XMLHttpRequest();
     //query lyrics
         // query sentiment
             // set mood 
-
-
